27e9e8d01ce1d4934d9d2a9164e105a560118ff1
Litogin <litogin1@gmail.com>
2025-09-21 11:06:51 +0300

    work UI

--- Content.Client/_WL/MetamorphosisSlime/Ui/MetamorphosisSlimeBoundUserInterface.cs
+++ Content.Client/_WL/MetamorphosisSlime/Ui/MetamorphosisSlimeBoundUserInterface.cs
@@ -1,11 +1,11 @@
-using Content.Client._WL.MetamorphosisSlime;
+using Content.Shared._WL.MetamorphosisSlime;
 using Content.Shared.Radio;
 using Content.Shared.Radio.Components;
 using JetBrains.Annotations;
 using Robust.Client.GameObjects;
 using Robust.Client.UserInterface;
 
-namespace Content.Client.Radio.Ui;
+namespace Content.Client._WL.MetamorphosisSlime.Ui;
 
 [UsedImplicitly]
 public sealed class MetamorphosisSlimeBoundUserInterface : BoundUserInterface
@@ -22,28 +22,24 @@ public sealed class MetamorphosisSlimeBoundUserInterface : BoundUserInterface
     {
         base.Open();
 
+        //var currentEmotion = PAIEmotion.Neutral;
+        //if (EntMan.TryGetComponent<PAIEmotionComponent>(Owner, out var emotionComp))
+        //    currentEmotion = emotionComp.CurrentEmotion;
+
         _menu = this.CreateWindow<MetamorphosisSlimeMenu>();
 
-        if (EntMan.TryGetComponent(Owner, out IntercomComponent? intercom))
+        if (EntMan.TryGetComponent(Owner, out MetamorphosisSlimeComponent? Metamorph))
         {
-            _menu.Update((Owner, intercom));
+            _menu.Update((Owner, Metamorph));
         }
 
-        _menu.OnMicPressed += enabled =>
-        {
-            SendMessage(new ToggleIntercomMicMessage(enabled));
-        };
-        _menu.OnSpeakerPressed += enabled =>
-        {
-            SendMessage(new ToggleIntercomSpeakerMessage(enabled));
-        };
-        _menu.OnChannelSelected += channel =>
-        {
-            SendMessage(new SelectIntercomChannelMessage(channel));
-        };
+        //_menu = new MetamorphosisSlimeMenu();
+        //_menu.OpenCentered();
+        //_menu.OnClose += Close;
+        //_menu.OnEmotionSelected += OnEmotionSelected;
     }
 
-    public void Update(Entity<IntercomComponent> ent)
+    public void Update(Entity<MetamorphosisSlimeComponent> ent)
     {
         _menu?.Update(ent);
     }
