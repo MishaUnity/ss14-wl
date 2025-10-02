1f65127ce323917701565d57a56431171c3c31f2
MishaUnity <81403616+MishaUnity@users.noreply.github.com>
2025-10-02 12:55:55 +0300

    Update RadarMarkerComponent.cs

--- Content.Shared/_WL/Shuttles/RadarMarkerComponent.cs
+++ Content.Shared/_WL/Shuttles/RadarMarkerComponent.cs
@@ -2,9 +2,14 @@ using Robust.Shared.GameStates;
 
 namespace Content.Shared._WL.Shuttles;
 
-[NetworkedComponent, RegisterComponent]
-public sealed class RadarMarkerComponent : Component
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class RadarMarkerComponent : Component
 {
-    [DataField]
+    [DataField, AutoNetworkedField]
     public bool Visible = true;
+
+    [DataField, AutoNetworkedField]
+    public Color Color = Color.Orange;
+    [DataField, AutoNetworkedField]
+    public float Size = 1f;
 }
